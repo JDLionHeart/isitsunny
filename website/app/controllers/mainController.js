ea1689ca3d9c9eb6ae6eb40ec48ef46639d617f4
Youssef Swailem <youssefswailem@gmail.com>
2017-01-22 19:28:30 +0200

    Created website that uses the data set

---
+++ website/app/controllers/mainController.js
@@ -0,0 +1,59 @@
+node_cj = require("node-csv-json");
+var _ = require('lodash');
+
+module.exports.getEpisodes = function(req, res){
+
+    node_cj({
+        input:  __dirname + "/Data.csv",
+        output: null
+    }, function(err, result){
+
+        if(err) {
+            console.error(err);
+        }
+
+        else {
+
+            var episodes = result;
+
+            var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+            var currentTime = new Date();
+            var currentHour = currentTime.getHours();
+            var currentMin = currentTime.getMinutes();
+            var currentDay = days[currentTime.getDay()];
+
+            //filter out episodes on a different day
+            episodes = episodes.filter(function(a){
+                return a.Day == currentDay;
+            });
+
+            if(episodes.length == 1){
+                return res.json(episodes[0]);
+            }
+
+            else{
+
+                episodes = episodes.sort(function(a, b){
+                    return Math.abs(currentHour - parseInt(a.Time.split(":")[0]))
+                    - Math.abs(currentHour - parseInt(b.Time.split(":")[0]));
+                });
+
+                //take first 2
+                episodes = _.take(episodes, 2);
+
+                episodes = episodes.sort(function(a, b){
+                    return Math.abs(currentMin - parseInt(a.Time.split(":")[1]))
+                    - Math.abs(currentMin - parseInt(b.Time.split(":")[1]));
+                })
+
+            }
+
+            return res.json(episodes[0]);
+        }
+
+    });
+
+
+
+}
---
+++ website/public/controllers/mainCtrl.js
@@ -0,0 +1,27 @@
+app.controller('mainCtrl', function($http, $scope){
+
+    $scope.getEpisode = function(){
+
+        $http({
+            method: "GET",
+            url: "/getData"
+        }).then(function(response){
+
+            console.log(response);
+            $scope.done = true;
+            var data = response.data;
+
+            var currDate = new Date();
+            $scope.currTime = currDate.getHours() + ":" + currDate.getMinutes() + " on a " + data.Day;
+            $scope.title = data.Title;
+            $scope.season = data.Season;
+            $scope.episode = data.Episode;
+            // $scope.day = data.Day;
+            $scope.time = data.Time + " on a " + data.Day;
+
+
+
+        });
+
+    }
+});
