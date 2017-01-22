e9b30921444778f5d7e3f0a94e8d24bb7972c956
Youssef Swailem <youssefswailem@gmail.com>
2017-01-22 20:05:07 +0200

    Added fade in animation

--- website/public/controllers/mainCtrl.js
+++ website/public/controllers/mainCtrl.js
@@ -2,25 +2,35 @@ app.controller('mainCtrl', function($http, $scope){
 
     $scope.getEpisode = function(){
 
-        $http({
-            method: "GET",
-            url: "/getData"
-        }).then(function(response){
+        $(document).ready(function(){
 
-            // console.log(response);
-            $scope.done = true;
-            var data = response.data;
+            $http({
+                method: "GET",
+                url: "/getData"
+            }).then(function(response){
 
-            var currDate = new Date();
-            $scope.currTime = currDate.getHours() + ":" + currDate.getMinutes() + " on a " + data.Day;
-            $scope.title = data.Title;
-            $scope.season = data.Season;
-            $scope.episode = data.Episode;
-            $scope.time = data.Time + " on a " + data.Day;
+                // console.log(response);
 
+                $scope.done = true;
+                var data = response.data;
+                var currDate = new Date();
 
+                var currentHour = currDate.getHours();
+                var currentMin = currDate.getMinutes();
 
+                if(currentMin < 10){
+                    currentMin = "0" + currentMin;
+                }
+
+                $scope.currTime = currentHour + ":" + currentMin + " on a " + data.Day;
+                $scope.title = data.Title;
+                $scope.season = data.Season;
+                $scope.episode = data.Episode;
+                $scope.time = data.Time + " on a " + data.Day;
+
+                $('#results').fadeIn(1000);
+
+            });
         });
-
     }
 });
